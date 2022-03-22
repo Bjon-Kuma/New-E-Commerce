8917ee3e5db801ea32bc4627edc6c16913379674
Bjon <Buschiazzomaximiliano3@gmail.com>
2022-03-22 12:28:25 -0300

    test

--- new-e-commerce/src/App.js
+++ new-e-commerce/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import Navbar from './components/Styles/Nav';
+import Button from 'react-bootstrap/Button';
 import './App.css';
+import Main from './components/Styles/Main';
+import Basket from './components/Styles/Basket';
+import data from './Data';
+import { useState } from 'react';
+import Product from './components/Styles/Product';
 
 function App() {
+  const {products} = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find(x => x.id === product.id);
+    if(exist) {
+      setCartItems(
+        cartItems.map((x) => 
+          x.id === product.id ? {...exist, qty: exist.qty +1} : x))
+    }
+    else{
+      setCartItems([...cartItems, {...product, qty: 1}])
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className='row'>
+        <Main products={products} /> 
+        <Basket cartItems={cartItems} />
+      </div>
+      
     </div>
   );
 }
---
+++ new-e-commerce/src/Data.js
@@ -0,0 +1,28 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name:'Macbook',
+            price: 1400,
+            image:'https://http2.mlstatic.com/D_NQ_NP_676118-MLA48625650348_122021-O.jpg'
+
+        },
+        {
+            id: '2',
+            name:'Quesadilla',
+            price: 1500,
+            image:'https://i.blogs.es/2a00ea/quesadilla_vertical/650_1200.jpg'
+
+        },
+        {
+            id: '3',
+            name:'Pizza',
+            price: 1600,
+            image:'https://img-global.cpcdn.com/recipes/75cf3c4170a22033/400x400cq70/photo.jpg'
+
+        },
+
+    ],
+};
+
+export default data
